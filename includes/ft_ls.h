43fa41e970cf68dcd2c1f8b1e0ea1037212d0dc6
Jerome HAMON <jhamon@e3r12p10.42.fr>
2019-04-13 18:50:38 +0200

    add .h

---
+++ includes/ft_ls.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhamon <jhamon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/13 17:47:47 by jhamon            #+#    #+#             */
+/*   Updated: 2019/04/13 17:55:33 by jhamon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+
+# define CH(X) printf("%50c-- check %d --\n", ' ', X);
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include "libft.h"
+
+
+#endif

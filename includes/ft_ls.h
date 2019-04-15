b2c7827026daea7d02c4e48bb93e7f583e5accb8
Jerome HAMON <jhamon@e3r12p10.42.fr>
2019-04-15 12:50:54 +0200

    main test

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: jhamon <jhamon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/13 17:47:47 by jhamon            #+#    #+#             */
-/*   Updated: 2019/04/13 17:55:33 by jhamon           ###   ########.fr       */
+/*   Updated: 2019/04/15 11:46:40 by jhamon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <time.h>
 #include "libft.h"
 
 
